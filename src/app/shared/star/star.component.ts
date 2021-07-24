b8d4b3555704d8d183b86033a79603ec3990d966
Madridista91 <a.jangelovski@yahoo.com>
2021-07-24 16:19:47 +0200

    first commit

---
+++ src/app/shared/star/star.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnInit, OnChanges {
+
+  @Input() rating: number = 0;
+  cropWidth: number = 75;
+  constructor() { }
+
+  ngOnChanges(): void {
+    this.cropWidth = this.rating * 75 / 5;
+
+  }
+
+  ngOnInit() {
+  }
+
+}
